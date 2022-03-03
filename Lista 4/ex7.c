f13822d5e7b68832a7b4df351b80b3e9cacfe50e
unknown <lorena.vasconcellos2003@gmail.com>
2022-03-03 12:19:47 -0300

    Lista 2 - ex 7

--- Lista 4/ex7.c
+++ Lista 4/ex7.c
@@ -1,13 +1,26 @@
 # include <stdio.h>
 # include <stdlib.h>
+#include <string.h>
 
 main () {
 
   char frase [100];
-  int cont = 0;
+  int cont_vogais = 0;
 
   printf ("Digite uma frase: \n");
-  scanf ("%d", &frase);
+  scanf ("%[^\n]", &frase);
+
+  for (int c = 0; c < strlen(frase); c++) {
+
+    if (frase[c] == 'a' || frase[c] == 'A' || frase[c] == 'e' || frase[c] == 'E' || frase[c] == 'i' || frase[c] == 'I' || frase[c] == 'o' || frase[c] == 'O' || frase[c] == 'u' || frase[c] == 'U') {
+      
+      for (int i = 0; i < cont_vogais; i++) {
+        printf ("%c", frase[c]);
+      }
+      cont_vogais += 1;
+    }
+    printf ("%c", frase[c]);
+  }
 
   return 0;
 }
