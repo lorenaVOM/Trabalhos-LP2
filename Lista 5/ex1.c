b80966e42ca4a4189cbbeebb05b6c406f06079d2
unknown <lorena.vasconcellos2003@gmail.com>
2022-03-21 23:02:13 -0300

    Lista 5 - att

--- Lista 5/ex1.c
+++ Lista 5/ex1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int maiusc (char *frase) {
+void maiusc (char *frase) {
 
   int i = 0;
 
@@ -10,22 +10,66 @@ int maiusc (char *frase) {
   }
 }
 
-int concatena (char *str1, char *str2, char *result) {
+char concatena (char *str1, char *str2) {
 
+int tam = strlen(str1);
+
+for (int i = 0; i < strlen(str2) + 1 ; i++) {
+  *(str1 + tam) = *(str2 + i);
+  tam++;
+}
+return str1;
 }
 
 int capit (char *frase) {
 
+  for (int i = 0; i < strlen(frase); i++) {
+    if (i == 0) {
+      if (*(frase + i) >= 97 && *(frase + i) <= 122) {
+        *(frase + i) -= 32;
+      }
+      }
+    else if (*(frase + i) == 32) {
+      if (*(frase + i + 1) >= 97 && *(frase + i + 1) <= 122) {
+        *(frase + i + 1) -= 32;
+        }
+    }
+  }
+    printf("\n%s\n", frase);
+    return strlen(frase);
 }
 
 int comp (char *str1, char *str2) {
 
+int i = 0, res = 0, menor;
+
+if (strlen(str1) < strlen(str2)) {
+  menor = strlen(str1);
+}
+else {
+  menor = strlen(str2);
+}
+
+for (i; i < menor; i++) {
+
+  if (*(str1 + i) < *(str2 + i)) {
+    res = -1;
+  }
+  else if (*(str1 + i) == *(str2 + i)) {
+    res = 0;
+  }
+  else if (*(str1 + i) > *(str2 + i)) {
+    res = 1;
+  }
+}
+printf ("%d", res);
+return res;
 }
 
 main () {
 
 char frase1 [30], frase2 [30];
-int cont = 0;
+int opcao, cont = 0;
 
 printf ("Digite uma frase: \n");
 gets(frase1);
@@ -33,15 +77,47 @@ gets(frase1);
 printf ("Digite outra frase: \n");
 gets(frase2);
 
-while (frase1[cont] != '\0') {
+printf ("\nEscolha o que deseja fazer:\n[1] Converter tudo para maíusculo (string 1)\n[2] Concatenação das frases\n[3] Capitalizar frase (string 1)\n[4] Comparação das duas frases\n");
+scanf ("%d", &opcao);
 
-  maiusc(&frase1[cont]);
-  printf("%c", frase1[cont]);
-  cont += 1;
+switch (opcao)
+{
+case 1:
 
+  while (frase1[cont] != '\0') {
+
+    maiusc(&frase1[cont]);
+    printf("%c", frase1[cont]);
+    cont += 1;
+
+  }
+  break;
+
+case 2:
+
+  concatena(frase1, frase2);
+  printf("\n%s", frase1);
+
+  break;
+
+case 3:
+
+  capit(frase1);
+
+  break;
+
+case 4:
+
+  comp(frase1, frase2);
+
+  break;
+
+default:
+
+  printf ("Essa opção não é válida");
+
+  break;
 }
-
-cont = 0;
 
 return 0;
 }
---
+++ Lista 5/ex2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int vetor[30];
+
+void zeraPares (int *vet) {
+
+for (int i = 0; i < 30; i++) {
+  if ((*vet) % 2 == 0) {
+    
+  }
+}
+}
+
+void dobra (int *vet) {
+
+}
+
+void soma (int *vet1, int *vet2) {
+
+}
+
+main () {
+
+  return 0;
+}
